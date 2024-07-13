714e29151498cbf48345ac70109ce6ea089edca6
o_thollas <o_thollas@hotmail.com>
2024-07-13 17:38:50 +0200

    creation header + menu hamburger

---
+++ public/asset/js/script.js
@@ -0,0 +1,29 @@
+
+let hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector(".nav");
+let bar = document.querySelector(".bar");
+
+/* script pour le bouton hamburger */
+
+hamburger.addEventListener("click", ()=>{
+    nav.classList.toggle("active");
+    bar.classList.toggle("active");
+    hamburger.classList.toggle("active");
+})
+
+/* script pour la navbar */
+
+let caroussel = document.querySelector(".caroussel");
+let gauche = document.getElementsByClassName("btn_gauche");
+let btnDroit = document.querySelector(".btn_droite");
+let x= 0;
+caroussel.addEventListener("click", function(){
+   
+})
+
+
+console.log(caroussel)
+
+/* script pour le carroussel */
+
+/* script pour le fun  */
