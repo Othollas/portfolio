65f36247f4502b0f935af281455f123934d61392
o_thollas <o_thollas@hotmail.com>
2024-07-14 23:03:29 +0200

    slider fini + modif formulaire contact style

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,9 +1,8 @@
+/* script pour le bouton hamburger */
 
 let hamburger = document.querySelector(".hamburger");
 let nav = document.querySelector(".nav");
 let bar = document.querySelector(".bar");
-
-/* script pour le bouton hamburger */
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
@@ -13,102 +12,47 @@ hamburger.addEventListener("click", () => {
 
 /* script pour le carroussel */
 
-
-
 let gauche = document.querySelector(".gauche");
 let droite = document.querySelector(".droite");
 let slide = document.querySelector(".contenu_carroussel");
 let tailleEcran;
-
-// condition pour definir la taille du slide selon la taille de l'ecran
-if (tailleEcran == undefined){
-    if(window.innerWidth < 800){
-        tailleEcran = window.innerWidth;
-    }else{
-        tailleEcran = 800;
-    }
-}
 
 const verifPositionImg = () => {
     let currentIndex = Math.floor(slide.scrollLeft / tailleEcran);
     slide.scrollLeft = currentIndex * tailleEcran;
 };
 
+//fonction pour definir le maximum de la limite du slider en px selon la taille de la fenetre
 
-window.addEventListener("resize", () => {
-    if (window.innerWidth < 800) {
-        tailleEcran = window.innerWidth;
-        return tailleEcran
-    }else{
-        tailleEcran = 800;
+const finSlide = () => {
+    return slide.scrollWidth - slide.clientWidth;
+};
+
+const adapterTailleEcran = ()=>{
+  tailleEcran =  window.innerWidth < 800 ? window.innerWidth : 800;
+  verifPositionImg();
+}
+
+// ternaire pour definir la taille du slide selon la taille de l'ecran
+window.innerWidth < 800 ? tailleEcran = window.innerWidth : tailleEcran = 800 ;
+
+window.addEventListener("resize", adapterTailleEcran)
+
+droite.addEventListener('click', () => {
+    slide.scrollLeft += tailleEcran;
+    if(slide.scrollLeft >= finSlide()){
+        slide.scrollLeft = 0;
     }
 })
 
-gauche.addEventListener('click', () => {
-    slide.scrollLeft += tailleEcran;
-    console.log(slide.clientWidth)
+gauche.addEventListener('click' || 'onTouch' , () => {
     
-})
-
-droite.addEventListener('click', () => {
     slide.scrollLeft -= tailleEcran;
-    console.log((slide.scrollWidth - slide.clientWidth))
+    if(slide.scrollLeft <= 0){
+        slide.scrollLeft = finSlide();
+    }
 })
-
-console.log(slide)
-
 
 window.addEventListener("resize", ()=>{
-    verifPositionImg();
+    
 })
-
-console.log(slide.currentIndex)
-/* script pour le fun  */
-
-// let gauche = document.querySelector(".gauche");
-// let droite = document.querySelector(".droite");
-// let slide = document.querySelector(".contenu_carroussel");
-// let tailleEcran = 800;
-
-// const updateScrollPosition = () => {
-//     let currentIndex = Math.floor(slide.scrollLeft / tailleEcran);
-//     slide.scrollLeft = currentIndex * tailleEcran;
-// };
-
-// const checkLoop = () => {
-//     const maxScrollLeft = slide.scrollWidth - slide.clientWidth;
-//     if (slide.scrollLeft >= maxScrollLeft) {
-//         slide.scrollLeft = 0;
-//     } else if (slide.scrollLeft <= 0) {
-//         slide.scrollLeft = maxScrollLeft;
-//     }
-// };
-
-// window.addEventListener("resize", () => {
-//     if (window.innerWidth < 800) {
-//         tailleEcran = window.innerWidth;
-//     } else {
-//         tailleEcran = 800;
-//     }
-//     updateScrollPosition();
-// });
-
-// gauche.addEventListener('click', () => {
-//     slide.scrollLeft -= tailleEcran;
-//     if (slide.scrollLeft < 0) {
-//         slide.scrollLeft = slide.scrollWidth - slide.clientWidth;
-//     }
-//     checkLoop();
-// });
-
-// droite.addEventListener('click', () => {
-//     slide.scrollLeft += tailleEcran;
-//     if (slide.scrollLeft >= slide.scrollWidth - slide.clientWidth) {
-//         slide.scrollLeft = 0;
-//     }
-//     checkLoop();
-// });
-
-// window.addEventListener("resize", () => {
-//     updateScrollPosition();
-// });
