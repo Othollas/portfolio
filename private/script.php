0997780bcba627d98ed93b1122bad1afe36928b1
o_thollas <o_thollas@hotmail.com>
2024-07-17 23:57:30 +0200

    ajout de IntersectionObserver pour les ecouteurs d'evenement

--- private/script.php
+++ private/script.php
@@ -1,1 +1,6 @@
 <!-- script php pour la reception du formulaire en POST et le traitement pour un envoit par mail.  -->
+ <?php 
+ 
+echo "hello world";
+
+ ?>
--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -10,6 +10,107 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
 })
 
+// script pour mettre le header en position fixe lors du scroll
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        header.classList = "scroll";
+    } else {
+        header.classList.remove("scroll");
+    }
+})
+
+// parametrage de l'api observer afin d'animer les elements au scroll
+
+
+
+let telechargement = document.querySelector(".telechargement");
+let logos = document.querySelectorAll(".logo");
+let carroussel = document.querySelector(".contenu_carroussel")
+let form = document.querySelector(".container-input")
+
+// const option = {
+//     root: document.querySelector("html"),
+//     rootMargin: "0px",
+//     threshold: 1.0,
+// }
+
+const observerTelechargement = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.animate([
+                { transform: "scale(1) rotate(0deg)", opacity: 0 },
+                { transform: "scale(1.5) rotate(-180deg)", opacity: 1 },
+                { transform: "scale(1) rotate(-360deg)", opacity: 1 }
+
+            ], { duration: 2000 })
+        }
+    }
+})
+
+const observerlogos = new IntersectionObserver((entries) => {
+
+})
+
+
+const observerCarroussel = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.animate([
+                { transform: "translateX(-100px)", opacity: 0 },
+                { transform: "translateX(0px)", opacity: 1 },
+            ], {duration: 1000})
+        }
+    }
+})
+
+
+const observerForm = new IntersectionObserver((entries) => {
+
+})
+
+const observer = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting && entry.target.classList.value == "telechargement") {
+            entry.target.animate([
+                { transform: 'scale(1)', opacity: 0 },
+                { transform: 'scale(1.5)', opacity: 1 },
+                { transform: 'scale(1)', opacity: 1 }
+            ],
+                { duration: 1000 }
+            )
+            entry.target.classList("flecheGauche").animate([
+                { transform: 'scale(1)', opacity: 0 },
+                { transform: 'scale(1.5)', opacity: 1 },
+                { transform: 'scale(1)', opacity: 1 }
+            ],
+                { duration: 1000 }
+            )
+        }
+        if (entry.isIntersecting && entry.target.classList.value == "container-input") {
+            entry.target.animate([
+                { transform: 'translateY(-20px)', opacity: 0 },
+                { transform: 'translateY(0px)', opacity: 1 },
+
+            ],
+                { duration: 1000 }
+            )
+            c
+        }
+    }
+},
+    {
+        threshold: 1
+    })
+
+// observer.observe(presentation);
+observer.observe(document.querySelector(".container-input"));
+observerTelechargement.observe(telechargement);
+observerCarroussel.observe(carroussel);
+
+
 /* script pour le carroussel */
 
 let gauche = document.querySelector(".gauche");
@@ -28,31 +129,27 @@ const finSlide = () => {
     return slide.scrollWidth - slide.clientWidth;
 };
 
-const adapterTailleEcran = ()=>{
-  tailleEcran =  window.innerWidth < 800 ? window.innerWidth : 800;
-  verifPositionImg();
+const adapterTailleEcran = () => {
+    tailleEcran = window.innerWidth < 800 ? window.innerWidth : 800;
+    verifPositionImg();
 }
 
 // ternaire pour definir la taille du slide selon la taille de l'ecran
-window.innerWidth < 800 ? tailleEcran = window.innerWidth : tailleEcran = 800 ;
+window.innerWidth < 800 ? tailleEcran = window.innerWidth : tailleEcran = 800;
 
 window.addEventListener("resize", adapterTailleEcran)
 
 droite.addEventListener('click', () => {
     slide.scrollLeft += tailleEcran;
-    if(slide.scrollLeft >= finSlide()){
+    if (slide.scrollLeft >= finSlide()) {
         slide.scrollLeft = 0;
     }
 })
 
-gauche.addEventListener('click' || 'onTouch' , () => {
-    
+gauche.addEventListener('click' || 'onTouch', () => {
+
     slide.scrollLeft -= tailleEcran;
-    if(slide.scrollLeft <= 0){
+    if (slide.scrollLeft <= 0) {
         slide.scrollLeft = finSlide();
     }
-})
-
-window.addEventListener("resize", ()=>{
-    
 })
